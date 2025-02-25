e431bb7a7a6f506f5f0e68cbdab86f61fa44b2ac
Aishwaryapol <polaishwarya78@gmail.com>
2025-02-25 21:19:51 +0530

    Fixed basic01project structure

---
+++ basic01project/src/Chai.js
@@ -0,0 +1,11 @@
+function Chai()
+{
+    return (
+        <>
+        <h3> chai and code with Ansh</h3>
+        <h1> hello ansh</h1>
+        <p>    this is paragraph</p>
+        </>
+    )
+}
+export default Chai
